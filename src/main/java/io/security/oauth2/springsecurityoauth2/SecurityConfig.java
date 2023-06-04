465be72b4ac40f306a10281c0b1998c53c5c727a
Ujung0622 <Ujung0622@users.noreply.github.com>
2023-06-04 18:17:14 +0900

    초기커밋2 - gitignore수정

---
+++ src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
@@ -0,0 +1,15 @@
+package io.security.oauth2.springsecurityoauth2;
+
+import org.springframework.http.converter.json.GsonBuilderUtils;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig {
+
+//    @Bean
+//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+//        http.authrize
+//    }
+
+
+}
