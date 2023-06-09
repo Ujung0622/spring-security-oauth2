a478bfddf068e66879c32894095f77af1cdd5ecf
Ujung0622 <Ujung0622@users.noreply.github.com>
2023-06-09 19:36:52 +0900

    cors 이해 끗

--- cors-1/src/main/java/io/security/cors1/Cors1Controller.java
+++ cors-1/src/main/java/io/security/cors1/Cors1Controller.java
@@ -4,9 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class CorsController {
+public class Cors1Controller {
     @GetMapping("/")
     public String index(){
+        System.out.println("여기 온거 맞지!!!!!!ㅇㅇㅇㅇㅇ?");
+
         return "/index";
     }
 }
--- cors-2/src/main/java/io/security/cors2/SecurityConfig.java
+++ cors-2/src/main/java/io/security/cors2/SecurityConfig.java
@@ -1,38 +1,38 @@
 package io.security.cors2;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.beans.BeanProperty;
 
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests().anyRequest().authenticated();
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll();
         http.cors().configurationSource(corsConfigurationSource());
 
         return http.build();
     }
-
     @Bean
-    public CorsConfigurationSource corsConfigurationSource(){
-
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("*");
+        configuration.addAllowedOrigin("http://localhost:8081");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
-        configuration.setAllowCredentials(true);
-        configuration.setMaxAge(3600L);
-
+        configuration.setAllowCredentials(true); // 인증을 사용하면 addAllowedOrigin에 *옵션을 줄 수 없음
+        configuration.setMaxAge(1L);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
 
         return source;
+
     }
 
 
